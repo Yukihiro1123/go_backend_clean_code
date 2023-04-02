c1c9f130822fcafdd23b150a178c28c8a7a47745
Yukihiro1123 <murasima1123@gmail.com>
2023-04-02 15:43:58 +0900

    refresh token

--- api/route/refresh_token_route.go
+++ api/route/refresh_token_route.go
@@ -1,2 +1,22 @@
 package route
 
+import (
+	"go_backend_clean_code/api/controller"
+	"go_backend_clean_code/bootstrap"
+	"go_backend_clean_code/domain"
+	"go_backend_clean_code/repository"
+	"go_backend_clean_code/usecase"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewRefreshTokenRouter(env *bootstrap.Env, timeout time.Duration, db mongo.Database, group *gin.RouterGroup) {
+	ur := repository.NewUserRepository(db, domain.CollectionUser)
+	rtc := &controller.RefreshTokenController{
+		RefreshTokenUsecase: usecase.NewRefreshTokenUsecase(ur, timeout),
+		Env: env,
+	}
+	group.POST("/refresh", rtc.RefreshToken)
+}
--- api/route/route.go
+++ api/route/route.go
@@ -13,6 +13,7 @@ func Setup(env *bootstrap.Env, timeout time.Duration, db mongo.Database, gin *gi
 	// All Public APIs
 	NewSignupRouter(env, timeout, db, publicRouter)
 	NewLoginRouter(env, timeout, db, publicRouter)
+	NewRefreshTokenRouter(env, timeout, db, publicRouter)
 	protectedRouter := gin.Group("")
 	// Middleware to verify AccessToken
 	protectedRouter.Use(middleware.JwtAuthMiddleware(env.AccessTokenSecret))
